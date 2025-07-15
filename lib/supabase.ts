be8b65e233289e24b49e63de7da77e84d6a7481c
seoiwon <smallcitydev@gmail.com>
2025-07-15 13:50:26 +0900

    ---vercel CI/CD

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://frontend-three-iota-69.vercel.app'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'dummy-key'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'dummy-keys'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
